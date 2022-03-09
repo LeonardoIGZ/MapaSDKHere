c2961740f24f2905e66b577a2ba1913f721b234d
Leonardo IGZ <leoguz079@hotmail.com>
2022-03-09 00:25:13 -0600

    Código para la rutas, aun no en funcionamiento

--- app/src/main/java/com/example/mapquest/MainActivity.java
+++ app/src/main/java/com/example/mapquest/MainActivity.java
@@ -9,15 +9,29 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.here.sdk.core.GeoCoordinates;
+import com.here.sdk.core.errors.InstantiationErrorException;
 import com.here.sdk.mapview.MapError;
 import com.here.sdk.mapview.MapScene;
 import com.here.sdk.mapview.MapScheme;
 import com.here.sdk.mapview.MapView;
 import com.here.sdk.mapview.VisibilityState;
+import com.here.sdk.routing.CalculateRouteCallback;
+import com.here.sdk.routing.CarOptions;
+import com.here.sdk.routing.Route;
+import com.here.sdk.routing.RoutingEngine;
+import com.here.sdk.routing.RoutingError;
+import com.here.sdk.routing.Section;
+import com.here.sdk.routing.SectionNotice;
+import com.here.sdk.routing.Waypoint;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
+    private RoutingEngine routingEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +53,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         loadMapScene();
-        mapView.getMapScene().setLayerVisibility(MapScene.Layers.LANDMARKS, VisibilityState.VISIBLE);
-        mapView.getMapScene().setLayerVisibility(MapScene.Layers.TRAFFIC_FLOW, VisibilityState.VISIBLE);
-        mapView.getMapScene().setLayerVisibility(MapScene.Layers.TRAFFIC_INCIDENTS, VisibilityState.VISIBLE);
-        mapView.getMapScene().setLayerVisibility(MapScene.Layers.SAFETY_CAMERAS, VisibilityState.VISIBLE);
-        mapView.getMapScene().setLayerVisibility(MapScene.Layers.VEHICLE_RESTRICTIONS, VisibilityState.VISIBLE);
-        mapView.getMapScene().setLayerVisibility(MapScene.Layers.EXTRUDED_BUILDINGS, VisibilityState.VISIBLE);
-        mapView.getMapScene().setLayerVisibility(MapScene.Layers.BUILDING_FOOTPRINTS, VisibilityState.VISIBLE);
 
+
+        rutas();
     }
 
     private void loadMapScene() {
@@ -63,6 +72,46 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void rutas(){
+        try {
+            routingEngine = new RoutingEngine();
+        } catch (InstantiationErrorException e) {
+            throw new RuntimeException("Initialization of RoutingEngine failed: " + e.error.name());
+        }
+
+        Waypoint startWaypoint = new Waypoint(new GeoCoordinates(52.530932, 13.384915));
+        Waypoint destinationWaypoint = new Waypoint(new GeoCoordinates(52.530932, 19.384915));
+
+        List<Waypoint> waypoints =
+                new ArrayList<>(Arrays.asList(startWaypoint, destinationWaypoint));
+
+        routingEngine.calculateRoute(
+                waypoints,
+                new CarOptions(),
+                new CalculateRouteCallback() {
+                    @Override
+                    public void onRouteCalculated(@Nullable RoutingError routingError, @Nullable List<Route> routes) {
+                        if (routingError == null) {
+                            Route route = routes.get(0);
+                            //showRouteDetails(route);
+                            //showRouteOnMap(route);
+                            logRouteViolations(route);
+                        } else {
+                            Log.d("Error calculating route", routingError.toString());
+                        }
+                    }
+                });
+
+    }
+
+    private void logRouteViolations(Route route) {
+        for (Section section : route.getSections()) {
+            for (SectionNotice notice : section.getSectionNotices()) {
+                Log.e(TAG, "This route contains the following warning: " + notice.code.toString());
+            }
+        }
     }
 
     @Override
